dbd7e5bc1737e43f857645fa530452294ba9f381
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-07-28 21:42:17 +0100

    update

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
+const calculate = require('./calculate')
+
+
 app.get("/", (req, res) => {
   res.send("Hello Devops - session 2 ... ");
+});
+
+app.post("/cal", (req, res) => {
+  const { a, b } = req.body
+  res.send({ "cal": calculate.cal(a, b) });
 });
 
 const port = 3000;
