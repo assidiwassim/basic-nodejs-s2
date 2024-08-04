07decac4f7bde39d4bb406054d14a1118172bc02
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-08-04 21:09:29 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Item = require("./models/Item");
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
+
+mongoose
+  .connect("mongodb://user:pass@51.77.210.239:27017/mydb", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Error = ",err));
+
+
 const calculate = require('./calculate')
+
+
+app.get("/items", (req, res) => {
+  Item.find()
+    .then((items) => res.send({ items }))
+    .catch((err) => res.status(500).json({ err }));
+});
+
+app.post("/items/add", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+  });
+
+  newItem
+    .save()
+    .then((item) => res.send({ message: "item saved successfully", item }))
+    .catch((err) => res.status(500).json({ err }));
+});
 
 
 app.get("/", (req, res) => {
